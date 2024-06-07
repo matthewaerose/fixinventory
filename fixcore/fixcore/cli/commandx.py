a8310d1a0657a390949a49401c0e1705f7b1dd18
Matthias Veit <matthias_veit@yahoo.de>
2024-06-07 21:53:41 +0200

    delete index

--- fixcore/fixcore/cli/commandx.py
+++ fixcore/fixcore/cli/commandx.py
@@ -20,34 +20,54 @@ class IdxCommand(CLICommand):
         return ""
 
     def parse(self, arg: Optional[str] = None, ctx: CLIContext = EmptyContext, **kwargs: Any) -> CLIAction:
-        props = arg.split(" ")
+        def create() -> CLISource:
+            props = arg.split(" ")
 
-        async def run() -> AsyncIterator[str]:
-            dba = self.dependencies.db_access
-            sdb = dba.database
-            vertex = sdb.collection(ctx.graph_name)
-            indexes = {idx["name"] for idx in vertex.indexes()}
-            section = ctx.section
-            # db = dba.get_graph_db(ctx.graph_name)
-            # model = await self.dependencies.model_handler.load_model(ctx.graph_name)
-            for prop in props:
-                idx_name = f"z_{prop}"
-                # term = P.array(prop).for_any.ne(None) if prop.endswith("]") else P.single(prop).ne(None)
-                # if idx_name not in indexes:
-                #     async with await db.search_list(
-                #         QueryModel(Query.by(term).on_section(Section.reported).with_limit(1), model)
-                #     ) as cursor:
-                #         if [item async for item in cursor]:
-                if idx_name not in indexes:
-                    try:
-                        vertex.add_persistent_index(
-                            [f"{section}.{prop}"], sparse=True, name=idx_name, in_background=False
-                        )
-                        yield f"Index created on {prop}"
-                    except Exception as e:
-                        yield f"Error creating index on {prop}: {e}"
+            async def run() -> AsyncIterator[str]:
+                dba = self.dependencies.db_access
+                sdb = dba.database
+                vertex = sdb.collection(ctx.graph_name)
+                indexes = {idx["name"] for idx in vertex.indexes()}
+                section = ctx.section
+                # db = dba.get_graph_db(ctx.graph_name)
+                # model = await self.dependencies.model_handler.load_model(ctx.graph_name)
+                for prop in props:
+                    idx_name = f"z_{prop}"
+                    # term = P.array(prop).for_any.ne(None) if prop.endswith("]") else P.single(prop).ne(None)
+                    # if idx_name not in indexes:
+                    #     async with await db.search_list(
+                    #         QueryModel(Query.by(term).on_section(Section.reported).with_limit(1), model)
+                    #     ) as cursor:
+                    #         if [item async for item in cursor]:
+                    if idx_name not in indexes:
+                        try:
+                            vertex.add_persistent_index(
+                                [f"{section}.{prop}"], sparse=True, name=idx_name, in_background=False
+                            )
+                            yield f"Index created on {prop}"
+                        except Exception as e:
+                            yield f"Error creating index on {prop}: {e}"
 
-        return CLISource.no_count(run)
+            return CLISource.no_count(run)
+
+        def delete() -> CLISource:
+
+            async def run() -> AsyncIterator[str]:
+                dba = self.dependencies.db_access
+                sdb = dba.database
+                vertex = sdb.collection(ctx.graph_name)
+                indexes = {idx["name"] for idx in vertex.indexes()}
+                for idx in indexes:
+                    if idx.startswith("z_"):
+                        try:
+                            vertex.delete_index(idx, ignore_missing=True)
+                            yield f"Index {idx} dropped"
+                        except Exception as e:
+                            yield f"Error dropping index {idx}: {e}"
+
+            return CLISource.no_count(run)
+
+        return delete()
 
 
 def all_commands(d: TenantDependencies) -> List[CLICommand]:
